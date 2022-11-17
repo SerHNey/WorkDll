5d3185c7cc942e92ece5e9ef2a3facd33d467c87
НаумовСА <naumov-1212@Mail.ru>
2022-11-17 14:50:05 +0300

    1%

--- Dllfile/Dllcode.cpp
+++ Dllfile/Dllcode.cpp
@@ -1,4 +1,11 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <Windows.h>
+#include <cstdio>
+#include "Header.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
 
 BOOL WINAPI DllMain(HINSTANCE hlnstDll, DWORD dwReason, LPVOID IpReserved) {
 	BOOL bAllWentWell = TRUE;
@@ -18,8 +25,33 @@ BOOL WINAPI DllMain(HINSTANCE hlnstDll, DWORD dwReason, LPVOID IpReserved) {
 	else
 		return FALSE;
 }
-extern "C" __declspec(dllimport) int MyFunc(LPWSTR str);
-int MyFunc(LPWSTR str) {
-	MessageBox(NULL, str, L"Я сделал", MB_OK);
-	return 0;
+extern "C" __declspec(dllimport) void MyFunc();
+
+
+
+void MyFunc() {
+
+
+	FILE* pFile = fopen("Users.csv", "r");
+	long size;
+	if (!pFile) perror("Error opening file");
+	else
+	{
+		fseek(pFile, 0, SEEK_END);
+		size = ftell(pFile);
+		fclose(pFile);
+		printf("Size of myfile.txt: %ld bytes.\n", size);
+		fclose(pFile);
+	}
+	HANDLE file = CreateFile(L"Users.csv", GENERIC_READ, NULL,0, OPEN_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
+	char* storoka =(char*) calloc(size+1, 1);
+	DWORD bytes;
+	ReadFile(file, storoka,size+1 , &bytes, NULL);
+	CloseHandle(file);
+	//char* person;
+	//person = strtok(storoka, " ");
+	file = CreateFile(L"resylt.csv", GENERIC_WRITE, NULL, 0, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
+	WriteFile(file, storoka, strlen(storoka), &bytes, NULL);
+
+
 }
--- WorkDll/WorkForDll.cpp
+++ WorkDll/WorkForDll.cpp
@@ -1,9 +1,16 @@
 #include <Windows.h>
+
 #define PATH L"Dllfile.dll"
+typedef void(_cdecl* MyFunc)();
+
+
 INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR lpCmdLine, INT nCmdShow)
 {
+
 	HINSTANCE hMyDll;
 	if ((hMyDll = LoadLibrary(PATH))==NULL) return 1;
+	MyFunc myf = GetProcAddress(hMyDll, "MyFunc");
+	myf("бсющ");
 
 
 	FreeLibrary(hMyDll);
